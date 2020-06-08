e1bb06d3d38253ccb3ba33c60d6cd1646000b49b
Alfonso Quevedo <alfonsoq6@gmail.com>
2020-06-07 17:13:34 -0700

    js for hi score list

--- end.js
+++ end.js
@@ -1,28 +1,30 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
-const mostRecentScore = localStorage.getItem("mostRecentScore");
 const finalScore = document.getElementById("finalScore");
-finalScore.innerText = mostRecentScore;
+const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
 });
 
-saveHighScore = (e) => {
+saveHighScore = e => {
   console.log("clicked the save button!");
   e.preventDefault();
+
   const score = {
     score: Math.floor(Math.random() * 100),
-    name: username.value,
+    name: username.value
   };
   highScores.push(score);
-
   highScores.sort((a, b) => b.score - a.score);
-
   highScores.splice(5);
 
   localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location, assign("/");
+  window.location.assign("/");
 };
